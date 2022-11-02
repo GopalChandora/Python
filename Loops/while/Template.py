43a911a7273910f83ae10c16802313c6138d2ae8
GopalChandora <gopalchandora38@gmail.com>
2022-11-02 19:06:08 +0530

    Template.py

---
+++ Loops/while/Template.py
@@ -0,0 +1,21 @@
+''' while expression:
+	statement
+	
+Example :- 1
+clg='terna'
+i=0
+while i<len(clg):
+	print(clg[i])
+	i+=1 '''
+
+n= int(input('Enter a number :'))
+sum=0
+print('sum of first',n,'number is:')
+i=0
+while i<n:
+	sum+=i
+	i+=1
+print('sum =',sum)
+
+
+	
