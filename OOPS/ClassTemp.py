5b8ed02a499549b0e2fb638e7b24d8a29b9f8caa
GopalChandora <gopalchandora38@gmail.com>
2022-11-02 19:19:12 +0530

    Create ClassTemp.py

---
+++ OOPS/ClassTemp.py
@@ -0,0 +1,17 @@
+#STNTAX
+
+'''class class_name:
+  statements
+  statements
+  
+object_name = class_name()'''
+
+#EXAMPLE
+
+class car:
+  name = "BMW"
+  price = "1cr"
+
+cr = car()
+print(cr.name)
+print(cr.price)
