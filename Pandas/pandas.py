8535aeaacf195325c17f4f4ee0d2b65d1fdcea9c
GopalChandora <gopalchandora38@gmail.com>
2023-08-08 20:36:46 +0530

    Uploaded Pandas Folder
    Two files - pandas.py and Exp10.py

---
+++ Pandas/pandas.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+#d1 = [ 1,2,3,4,5]
+#a=p.Series(d1)
+#print(a)
+
+#d1 = [['A',2],['B',3]]
+#b = p.DataFrame(d1,columns=['id ','Age'])
+#print(b)
+
+#l1= { 'c1':p.Series([1,2,3], index=['a','b','c'])}
+#new = p.DataFrame(l1,columns=['c1'])
+#print(new)
+
+
+list1 = pd.DataFrame('list',{'id no':[1,2],'name':['gopal','arvind']})
+print(list1)
